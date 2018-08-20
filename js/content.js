de0537942c92cbb80c48a12a1872cc79ad43685d
ThangPhanCong <pcthangk59@gmail.com>
2018-08-20 16:24:11 +0700

    prevent access page

--- js/content.js
+++ js/content.js
@@ -4,11 +4,11 @@ chrome.storage.local.set({ idCurrentRecieveUser }, function () {
 });
 
 $('body').on('click', '._1mf._1mj', function (event) {
-  var listClass = $(this).closest('._5qi9._5qib').find('._3_9e._s0f').attr('class').split(' ')
+  let listClass = $(this).closest('._5qi9._5qib').find('._3_9e._s0f').attr('class').split(' ')
   idCurrentRecieveUser = listClass[listClass.length-1].split(':')[1]
-  console.log('Current new', idCurrentRecieveUser);
+  // console.log('Current new', idCurrentRecieveUser);
   chrome.storage.local.set({ "idCurrentRecieveUser": idCurrentRecieveUser }, function () {
-    console.log('Value is set to ' + idCurrentRecieveUser);
+    // console.log('Value is set to ' + idCurrentRecieveUser);
   });
   // chrome.runtime.sendMessage(chrome.runtime.id, { newID: idCurrentRecieveUser })
 })
