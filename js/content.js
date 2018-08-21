2982da1d9cc0861f2beaa8f07363f6eb973ff715
Sotatek-ManhPham <manh.pham@sotatek.com>
2018-08-21 14:08:20 +0700

    Fix select user recieve message

--- js/content.js
+++ js/content.js
@@ -1,14 +1,21 @@
 var idCurrentRecieveUser = ''
 chrome.storage.local.set({ idCurrentRecieveUser }, function () {
-  console.log('Value is set to ' + idCurrentRecieveUser);
+  console.log('Value is set to ' + idCurrentRecieveUser)
 });
 
-$('body').on('click', '._1mf._1mj', function (event) {
-  let listClass = $(this).closest('._5qi9._5qib').find('._3_9e._s0f').attr('class').split(' ')
-  idCurrentRecieveUser = listClass[listClass.length-1].split(':')[1]
-  // console.log('Current new', idCurrentRecieveUser);
-  chrome.storage.local.set({ "idCurrentRecieveUser": idCurrentRecieveUser }, function () {
-    // console.log('Value is set to ' + idCurrentRecieveUser);
-  });
-  // chrome.runtime.sendMessage(chrome.runtime.id, { newID: idCurrentRecieveUser })
+$('body').on('click', function (event) {
+  var $currentTarget = $(event.target)
+  console.log('Class',$currentTarget.attr('class'))
+  if($currentTarget.hasClass('_1mf') && $currentTarget.hasClass('_1mj')) {
+    
+    var listClass = $currentTarget.closest('._5qi9._5qib').find('._3_9e._s0f').attr('class').split(' ')
+    idCurrentRecieveUser = listClass[listClass.length-1].split(':')[1]
+    chrome.storage.local.set({ "idCurrentRecieveUser": idCurrentRecieveUser }, function () {
+      console.log('Value is set to ' + idCurrentRecieveUser)
+    });
+  }else {
+    chrome.storage.local.set({ "idCurrentRecieveUser": "" }, function () {
+      console.log('Clear info user')
+    });
+  }
 })
